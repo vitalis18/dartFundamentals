19ee4aadc4187be8241d9531aead2c05c0b47d3b
UttamkiniH <53141266+UttamkiniH@users.noreply.github.com>
2021-06-21 11:36:39 +0530

    Create everything.dart

---
+++ everything.dart
@@ -0,0 +1,3 @@
+// to add annotations like doesn't we cannot add this as string value so we need to add \ before the ' example doesn\'t
+//using [ ] to enter the list of value 
+var questions = ['what are you doing?','aren\'t you?'];
